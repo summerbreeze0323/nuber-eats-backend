af9dd7f8c4f95c5d263a484a289f92dadb705706
Julee Kim <summerbreeze0323@gmail.com>
2021-02-26 15:28:39 +0900

    환경변수 사용, TypeOrmModule 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,15 +8,17 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
   imports: [
     ConfigModule.forRoot({ // .env 사용을 위해
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test'
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod' // 서버에 deploy 할 떄 환경변수 파일을 사용하지 않는다는 옵션
+      // production 환경일 때는 ConfigModule이 환경변수 파일을 무시하게 됨
     }),
     TypeOrmModule.forRoot({
-      'type': 'postgres',
-      'host': 'localhost',
-      'port': 5432,
-      'username': 'julee',
-      'password': 'test', // 'host': 'localhost'로 되어있다면 패스워드를 묻지 않음
-      'database': 'nuber-eats',
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD, // 'host': 'localhost'로 되어있다면 패스워드를 묻지 않음
+      database: process.env.DB_DATABASE,
       'synchronize': true, // TypeORM이 db에 연결할 때 db를 나의 모듈의 현재 상태로 마이그래이션 한다는 뜻
       'logging': true, // db에서 무슨일이 일어나는지 콘솔로 표시
     }),
