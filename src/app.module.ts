15ed0cd84b059ade8146e552d53bec4c32945f41
Julee Kim <summerbreeze0323@gmail.com>
2021-02-26 15:41:14 +0900

    joi 모듈 추가, Validating ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi'; // 자바스크립트로 만들어진 모듈이기 때문에 이렇게 import
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -9,8 +10,17 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
     ConfigModule.forRoot({ // .env 사용을 위해
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
-      ignoreEnvFile: process.env.NODE_ENV === 'prod' // 서버에 deploy 할 떄 환경변수 파일을 사용하지 않는다는 옵션
+      // ignoreEnvFile: 서버에 deploy 할 떄 환경변수 파일을 사용하지 않는다는 옵션
       // production 환경일 때는 ConfigModule이 환경변수 파일을 무시하게 됨
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      })
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
