ed5189e125ebe7781ff360a66b613f1c39e24997
Julee Kim <summerbreeze0323@gmail.com>
2021-03-02 19:46:43 +0900

    Update Restaurant

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Restaurant } from "./entities/restaurant.entity";
+import { UpdateRestaurantDto } from "./dtos/update.restaurant.dto";
 
 @Injectable()
 export class RestaurantService {
@@ -18,5 +19,9 @@ export class RestaurantService {
   createRestaurant(createRestaurantDto: CreateRestaurantDto): Promise<Restaurant> {
     const newRestaurant = this.restaurants.create(createRestaurantDto);
     return this.restaurants.save(newRestaurant);
+  }
+
+  updateRestaurant({id, data}: UpdateRestaurantDto) {
+    return this.restaurants.update(id, {...data});
   }
 }
