629153d1b4764748b95bc97717ccea0d3d0b4497
Julee Kim <summerbreeze0323@gmail.com>
2021-03-03 10:45:03 +0900

    Create Account Mutation part Two

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { CreateAccountInput } from "./dtos/create-account.dto";
 import { User } from "./entities/user.entity";
 
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private readonly users: Repository<User>) {}
+
+  async createAccount({email, password, role}: CreateAccountInput) {
+    // check new user
+    try {
+      const exists = await this.users.findOne({ email });
+      if (exists) {
+        // make error
+        return;
+      }
+      // create user
+      await this.users.save(this.users.create({ email, password, role }));
+      return true;
+    } catch (e) {
+      // make rror
+      return;
+    }
+    // hash the password
+  }
 }
