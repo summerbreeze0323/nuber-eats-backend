28d5011f3e4c6cad98bc4947e44bd6efab5d53cf
Julee Kim <summerbreeze0323@gmail.com>
2021-03-03 19:07:11 +0900

    GraphQL Context

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Mutation, Args } from "@nestjs/graphql";
+import { Resolver, Query, Mutation, Args, Context } from "@nestjs/graphql";
 import { CreateAccountInput, CreateAccountOutput } from "./dtos/create-account.dto";
 import { LoginInput, LoginOutput } from "./dtos/login.dto";
 import { User } from "./entities/user.entity";
@@ -38,7 +38,11 @@ export class UsersResolver {
   }
 
   @Query(returns => User)
-  me() {
-    
+  me(@Context() context) {
+    if (!context.user) {
+      return;
+    } else {
+      return context.user;
+    }
   }
 }
