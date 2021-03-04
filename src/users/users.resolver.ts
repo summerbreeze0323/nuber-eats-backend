83bbc1563d0e8625aa9d91aaf34e125991a743c2
Julee Kim <summerbreeze0323@gmail.com>
2021-03-04 11:24:54 +0900

    userProfile Mutation

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -4,6 +4,7 @@ import { AuthUser } from "src/auth/auth-user.decorator";
 import { AuthGuard } from "src/auth/auth.guard";
 import { CreateAccountInput, CreateAccountOutput } from "./dtos/create-account.dto";
 import { LoginInput, LoginOutput } from "./dtos/login.dto";
+import { UserProfileInput, UserProfileOutput } from "./dtos/user-profile.dto";
 import { User } from "./entities/user.entity";
 import { UsersService } from "./users.service";
 
@@ -39,5 +40,24 @@ export class UsersResolver {
   @UseGuards(AuthGuard)
   me(@AuthUser() authUser: User) {
     return authUser;
+  }
+
+  @UseGuards(AuthGuard)
+  @Query(returns => UserProfileOutput)
+  async userProfile(@Args() userProfileInput: UserProfileInput): Promise<UserProfileOutput> {
+    try {
+      const user = await this.usersService.findById(userProfileInput.userId);
+      if (!user) throw Error();
+
+      return {
+        ok: true,
+        user
+      }
+    } catch (error) {
+      return {
+        ok: false,
+        error: 'User Not Found.'
+      }
+    }
   }
 }
