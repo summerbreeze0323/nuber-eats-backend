ff4c99120d052d955bc0325e5230118363230be9
Julee Kim <summerbreeze0323@gmail.com>
2021-03-08 14:13:47 +0900

    Setting Up Tests

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,25 @@
+import { Test } from "@nestjs/testing";
+import { UsersService } from "./users.service";
+
+describe('UserService', () => {
+  let service: UsersService
+
+  // 테스트 모듈 생성
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      providers: [UsersService]
+    }).compile();
+
+    service = module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  })
+
+  it.todo('createAccount');
+  it.todo('login');
+  it.todo('findById');
+  it.todo('editProfile');
+  it.todo('verifyEmail');
+})
