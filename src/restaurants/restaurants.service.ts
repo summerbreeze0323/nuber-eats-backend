fcf6bb825d73dec46629cd7eb8bd006d9c64f737
Julee Kim <summerbreeze0323@gmail.com>
2021-03-11 15:20:19 +0900

    createRestaurant part Two

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -4,12 +4,15 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Restaurant } from "./entities/restaurant.entity";
 import { User } from "src/users/entities/user.entity";
+import { Category } from "./entities/category.entity";
 
 @Injectable()
 export class RestaurantService {
   constructor(
     @InjectRepository(Restaurant) // db 접근
-    private readonly restaurants: Repository<Restaurant>
+    private readonly restaurants: Repository<Restaurant>,
+    @InjectRepository(Category)
+    private readonly categories: Repository<Category>
   ) { }
 
   async createRestaurant(
@@ -18,6 +21,18 @@ export class RestaurantService {
   ): Promise<CreateRestaurantOutput> {
     try {
       const newRestaurant = this.restaurants.create(createRestaurantInput);
+      newRestaurant.owner = owner;
+      const categoryName = createRestaurantInput.categoryName.trim().toLowerCase();
+      const categorySlug = categoryName.replace(/ /g, '-');
+      let category = await this.categories.findOne({ slug: categorySlug });
+      if (!category) {
+        category = await this.categories.save(this.categories.create({
+          slug: categorySlug,
+          name: categoryName
+        }));
+      }
+      newRestaurant.category = category;
+
       await this.restaurants.save(newRestaurant);
       return { ok: true };
     } catch (error) {
