87d82943357d2b5f5dcda0373739e850df7c724d
Julee Kim <summerbreeze0323@gmail.com>
2021-03-11 13:40:45 +0900

    Restaurant Models

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,8 @@ import { JwtModule } from './jwt/jwt.module';
 import { JwtMiddleware } from './jwt/jwt.middleware';
 import { Verification } from './users/entities/verification.entity';
 import { MailModule } from './mail/mail.module';
+import { Restaurant } from './restaurants/entities/restaurant.entity';
+import { Category } from './restaurants/entities/category.entity';
 
 @Module({
   imports: [
@@ -40,7 +42,7 @@ import { MailModule } from './mail/mail.module';
       database: process.env.DB_DATABASE,
       synchronize: process.env.NODE_ENV !== 'prod', // TypeORM이 db에 연결할 때 db를 나의 모듈의 현재 상태로 마이그래이션 한다는 뜻
       logging: process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test', // db에서 무슨일이 일어나는지 콘솔로 표시
-      entities: [User, Verification]
+      entities: [User, Verification, Restaurant, Category]
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,40 +1,40 @@
 // Entity: db에 저장되는 데이터의 형태를 보여주는 모델
 import { Field, InputType, ObjectType } from "@nestjs/graphql";
 import { IsBoolean, IsOptional, IsString, Length } from "class-validator";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { CoreEntity } from "src/common/entities/core.entity";
+import { Column, Entity, ManyToOne } from "typeorm";
+import { Category } from "./category.entity";
 
 // @InputType({isAbstract: true}) // isAbstract: true는 InputType을 schema에 적용시키는 것은 원하지 않을 때 사용
 @ObjectType() // @ObjectType():자동으로 스키마를 빌드하기 위해 사용하는 GraphQL decorator
 @Entity() // for TypeORM
-export class Restaurant {
-  @PrimaryGeneratedColumn()
-  @Field(type => Number)
-  id: number;
-
+export class Restaurant extends CoreEntity {
   @Field(type => String) // for graphql
   @Column() // for database
   @IsString() // 여기부터는 for validation
-  @Length(5, 30) // min: 5, max: 30
+  @Length(5) // min: 5, max: 30
   name: string;
 
-  @Field(type => Boolean, { nullable: true }) // graphql playground DOCS에서 '!'값이 없다면 null 값을 허용하는 옵셔널(nullable) 값
-  @Column({ default: true })
-  @IsOptional()
-  @IsBoolean()
-  isVegan?: boolean;
+  // @Field(type => Boolean, { nullable: true }) // graphql playground DOCS에서 '!'값이 없다면 null 값을 허용하는 옵셔널(nullable) 값
+  // @Column({ default: true })
+  // @IsOptional()
+  // @IsBoolean()
+  // isVegan?: boolean;
 
   @Field(type => String, { defaultValue: '강남' })
   @Column()
   @IsString()
   address: string;
 
-  // @Field(type => String)
-  // @Column()
-  // @IsString()
-  // ownerName: string;
+  @Field(type => String)
+  @Column()
+  @IsString()
+  coverImg: string;
 
-  // @Field(type => String)
-  // @Column()
-  // @IsString()
-  // categoryName: string;
+  @Field(type => Category)
+  @ManyToOne(
+    type => Category,
+    category => category.restaurants,
+  )
+  category: Category;
 }
