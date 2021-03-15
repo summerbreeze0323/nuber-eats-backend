cd66f7e7e97220c1fb5c7c532642cd1a16051454
Julee Kim <summerbreeze0323@gmail.com>
2021-03-15 23:05:07 +0900

    Create Order part Six

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -21,44 +21,74 @@ export class OrderService {
     customer: User,
     { restaurantId, items }: CreateOrderInput
   ): Promise<CreateOrderOutput> {
-    const restaurant = await this.restaurants.findOne(restaurantId);
+    try {
+      const restaurant = await this.restaurants.findOne(restaurantId);
 
-    if (!restaurant) {
-      return {
-        ok: false,
-        error: 'Restaurant not found.'
+      if (!restaurant) {
+        return {
+          ok: false,
+          error: 'Restaurant not found.'
+        }
       }
-    }
 
-    for (const item of items) {
-      const dish = await this.dishes.findOne(item.dishId);
+      let orderFinalPrice = 0;
+      const orderItems: OrderItem[] = [];
+      for (const item of items) {
+        const dish = await this.dishes.findOne(item.dishId);
 
-      if (!dish) {
-        return { ok: false, error: 'Dish not found.' }
-      }
-      
-      console.log(`Dish price: ${dish.price}`);
-      for (const itemOption of item.options) {
-        const dishOption = dish.options.find(
-          dishOption => dishOption.name === itemOption.name
-        );
+        if (!dish) {
+          return { ok: false, error: 'Dish not found.' }
+        }
+        
+        console.log(`Dish price: ${dish.price}`);
+        let dishFinalPrice = dish.price;
+        for (const itemOption of item.options) {
+          const dishOption = dish.options.find(
+            dishOption => dishOption.name === itemOption.name
+          );
 
-        if (dishOption) {
-          if (dishOption.extra) {
-            console.log(`$USD + ${dishOption.extra}`);
-          } else {
-            const dishOptionChoice = dishOption.choices.find(
-              optionChoice => optionChoice.name === itemOption.choice
-            );
+          if (dishOption) {
+            if (dishOption.extra) {
+              console.log(`$USD + ${dishOption.extra}`);
+              dishFinalPrice = dishFinalPrice + dishOption.extra;
+            } else {
+              const dishOptionChoice = dishOption.choices.find(
+                optionChoice => optionChoice.name === itemOption.choice
+              );
 
-            if (dishOptionChoice) {
-              if (dishOptionChoice.extra) {
-                console.log(`$USD + ${dishOptionChoice.extra}`);
+              if (dishOptionChoice) {
+                if (dishOptionChoice.extra) {
+                  console.log(`$USD + ${dishOptionChoice.extra}`);
+                  dishFinalPrice = dishFinalPrice + dishOptionChoice.extra;
+                }
               }
             }
           }
         }
-      }
-    };
+
+        orderFinalPrice = orderFinalPrice + dishFinalPrice;
+
+        const orderItem = await this.orderItems.save(
+          this.orderItems.create({
+            dish,
+            options: item.options
+          })
+        );
+        orderItems.push(orderItem);
+      };
+
+      await this.orders.save(
+        this.orders.create({
+          customer,
+          restaurant,
+          total: orderFinalPrice,
+          items: orderItems
+        })
+      );
+
+      return { ok: true };
+    } catch (error) {
+      return { ok: false, error: 'Could not create' }
+    }
   }
 }
