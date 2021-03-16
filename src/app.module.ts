012e3438874fabbafbda289e9d596e201d67d6e5
Julee Kim <summerbreeze0323@gmail.com>
2021-03-16 09:40:01 +0900

    Subscriptions part Two

--- src/app.module.ts
+++ src/app.module.ts
@@ -53,9 +53,12 @@ import { OrderItem } from './orders/entities/order-item.entity';
     GraphQLModule.forRoot({
       installSubscriptionHandlers: true, // 웹 소켓 기능 사용
       autoSchemaFile: true,
-      context: ({ req }) => {
-        console.log(req);
-        return { user: req['user'] }
+      context: ({ req, connection }) => {
+        if (req) {
+          return { user: req['user'] }
+        } else {
+          console.log(connection);
+        }
       }
     }),
     JwtModule.forRoot({
