ba913eb27ad98235eed78b95d0424f75cbb35d67
Julee Kim <summerbreeze0323@gmail.com>
2021-03-16 23:00:15 +0900

    Promoting Restaurants part Two

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@nestjs/common";
-import { Cron, SchedulerRegistry, Interval, Timeout } from '@nestjs/schedule';
+import { Cron, Interval } from '@nestjs/schedule';
 import { InjectRepository } from "@nestjs/typeorm";
 import { Restaurant } from "src/restaurants/entities/restaurant.entity";
 import { User } from "src/users/entities/user.entity";
-import { Repository } from "typeorm";
+import { LessThan, Repository } from "typeorm";
 import { CreatePaymentInput, CreatePaymentOutput } from "./dtos/create-payment.dto";
 import { GetPaymentsOutput } from "./dtos/get-payments.dto";
 import { Payment } from "./entities/payment.entity";
@@ -55,5 +55,19 @@ export class PaymentService {
         error: 'Could not load payments.'
       }
     }
+  }
+
+  @Interval(2000)
+  async checkPromotedRestaurants() {
+    const restaurants = await this.restaurants.find({
+      isPromoted: true,
+      promotedUntil: LessThan(new Date())
+    });
+    console.log(restaurants);
+    restaurants.forEach(async restaurant => {
+      restaurant.isPromoted = false;
+      restaurant.promotedUntil = null;
+      await this.restaurants.save(restaurant);
+    });
   }
 }
