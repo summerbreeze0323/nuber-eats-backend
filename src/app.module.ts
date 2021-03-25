cf261ba0ec4d9b9d391ac24c063aac32fb960e6f
Julee Kim <summerbreeze0323@gmail.com>
2021-03-25 18:26:30 +0900

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -51,8 +51,8 @@ import { UploadsModule } from './uploads/uploads.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD, // 'host': 'localhost'로 되어있다면 패스워드를 묻지 않음
       database: process.env.DB_DATABASE,
-      synchronize: process.env.NODE_ENV !== 'prod', // TypeORM이 db에 연결할 때 db를 나의 모듈의 현재 상태로 마이그래이션 한다는 뜻
-      logging: process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test', // db에서 무슨일이 일어나는지 콘솔로 표시
+      synchronize: process.env.NODE_ENV !== 'production', // TypeORM이 db에 연결할 때 db를 나의 모듈의 현재 상태로 마이그래이션 한다는 뜻
+      logging: process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test', // db에서 무슨일이 일어나는지 콘솔로 표시
       entities: [User, Verification, Restaurant, Category, Dish, Order, OrderItem, Payment],
       // ssl: { rejectUnauthorized: false }
     }),
