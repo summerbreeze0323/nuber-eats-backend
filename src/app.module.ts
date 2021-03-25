9f91f174bd36dcc8ce5cff644a3f71db990a50d9
Julee Kim <summerbreeze0323@gmail.com>
2021-03-25 17:44:15 +0900

    edit

--- src/app.module.ts
+++ src/app.module.ts
@@ -53,7 +53,8 @@ import { UploadsModule } from './uploads/uploads.module';
       database: process.env.DB_DATABASE,
       synchronize: process.env.NODE_ENV !== 'prod', // TypeORM이 db에 연결할 때 db를 나의 모듈의 현재 상태로 마이그래이션 한다는 뜻
       logging: process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test', // db에서 무슨일이 일어나는지 콘솔로 표시
-      entities: [User, Verification, Restaurant, Category, Dish, Order, OrderItem, Payment]
+      entities: [User, Verification, Restaurant, Category, Dish, Order, OrderItem, Payment],
+      ssl: true
     }),
     GraphQLModule.forRoot({
       installSubscriptionHandlers: true, // 웹 소켓 기능 사용
